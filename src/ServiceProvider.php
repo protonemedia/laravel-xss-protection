506b2033c2e05d0aa4a6870197dfe8008828407e
Pascal Baljet <pascal@protone.media>
2025-08-09 17:04:15 +0200

    Make `voku/anti-xss` a direct dependency

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace ProtoneMedia\LaravelXssProtection;
 
-use GrahamCampbell\SecurityCore\Security;
+use ProtoneMedia\LaravelXssProtection\Middleware\XssCleanInput;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use voku\helper\AntiXSS;
 
 class ServiceProvider extends PackageServiceProvider
 {
@@ -22,9 +23,29 @@ class ServiceProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
-        $this->app->singleton(Security::class, fn () => Security::create(
-            config('xss-protection.anti_xss.evil'),
-            config('xss-protection.anti_xss.replacement')
-        ));
+        $this->app->when(XssCleanInput::class)
+            ->needs(AntiXSS::class)
+            ->give(function () {
+                $antiXss = new AntiXSS;
+
+                $replacement = config('xss-protection.anti_xss.replacement');
+
+                if ($replacement !== null) {
+                    $antiXss->setReplacement($replacement);
+                }
+
+                $evil = config('xss-protection.anti_xss.evil');
+
+                if ($evil !== null) {
+                    if (isset($evil['attributes']) || isset($evil['tags'])) {
+                        $antiXss->addEvilAttributes($evil['attributes'] ?? []);
+                        $antiXss->addEvilHtmlTags($evil['tags'] ?? []);
+                    } else {
+                        $antiXss->addEvilAttributes($evil);
+                    }
+                }
+
+                return $antiXss;
+            });
     }
 }
